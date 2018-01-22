b4e62e9d0c1eb402e9b515aca6a33d4c2577dc73
Daniyar Seferov <seferovdaniyar@gmail.com>
2018-01-22 16:08:44 +0000

    automatically backed up by learn

--- konami_code.js
+++ konami_code.js
@@ -4,7 +4,7 @@ const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
 function init() {
   let index = 0;
   const body = document.querySelector('body')
-  body.addEventListener('click', onKeyDownHandler)
+  body.addEventListener('keydown', onKeyDownHandler)
   
   function onKeyDownHandler(e) {
   const key = parseInt(e.detail || e.which);
