6d425b1c55658d39b43d4258043e90224ca71e12
Daniyar Seferov <seferovdaniyar@gmail.com>
2018-01-22 15:56:24 +0000

    automatically backed up by learn

--- konami_code.js
+++ konami_code.js
@@ -2,8 +2,8 @@ const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
 let index = 0;
 
 function init() {
-  const divs = document.querySelector('body')
- 
+  const body = document.querySelector('body')
+  body.addEventListener('click', onKeyDownHandler)
   
 }
 
